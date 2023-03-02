bf89f2af0462fa841dcc99536d76e956e2488426
CastellucciLuca <castellucciluca98@gmail.com>
2023-03-02 14:59:28 +0100

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['author', 'slug', 'title', 'post_date', 'content', 'image','type_id'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function isImageAUrl(){
+    return filter_var($this->image, FILTER_VALIDATE_URL);
+}
+    public function Technologies(){
+    return $this->belongsToMany(Technology::class);
+}
+}
