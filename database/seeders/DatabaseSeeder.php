cc2bee9620465c9d88796c20cfcd06ed5f68cb6e
CastellucciLuca <castellucciluca98@gmail.com>
2023-03-06 15:34:07 +0100

    Create roles tables model and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             TechnologySeeder::class,
             PostSeeder::class,
             PostTechnologySeeder::class,
+            RoleSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'level' => 1,
+                'name' => 'Admin',
+            ],
+            [
+                'level' => 2,
+                'name' => 'User',
+            ],
+        ];
+        foreach ($roles as $role) {
+            $newRole = new Role();
+            $newRole->name = $role['level'];
+            $newRole->name=$role['name'];
+            $newRole->save();
+        }
+    }
+}
